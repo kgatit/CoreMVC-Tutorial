4f8d802e3009d9dea310e1df07540a8a52b8bb2d
ALCHEMY\tebogo <tebogo@alchemysoftware.co.za>
2019-11-05 08:43:59 +0200

    Modified the welcome action method to demostrate MVC model binding system's automatically mapping the named parameters from the query string.

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,9 +18,10 @@ namespace MvcMovie.Controllers
 
         //
         // GET: /HelloWorld/Welcome/
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
